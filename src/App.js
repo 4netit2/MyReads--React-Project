1da1eee56ee0542c34c43972be02429c3d4c6966
BogdanP <4netit2@gmail.com>
2018-07-20 15:35:09 +0300

    get all books

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import * as BooksAPI from './BooksAPI'
-//import {Link} from 'react-router-dom'
+//import * as BooksAPI from './BooksAPI'
 
 import './App.css'
 
@@ -15,11 +14,6 @@ class BooksApp extends React.Component {
     showSearchPage: false
   }
   
-  componentDidMount() {
-    BooksAPI.getAll().then(books => console.log(books))
-    //let query = 'react';
-    //BooksAPI.search(query).then(books => console.log(books))
-}
   render() {
     return (
       <div className="app">
